6b92f4836f343af41f034623a9d9768a7f95c439
LatropAgency <vovakolesnikovich@gmail.com>
2020-08-06 18:55:18 +0300

    columns

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
     $(".menu__icon").click(function () {
-        $('.menu__icon').toggleClass( "active");
-        $('.menu__body').toggleClass( "active");
+        $('.menu__icon').toggleClass("active");
+        $('.menu__body').toggleClass("active");
+        $('body').toggleClass("lock");
     });
+
+    function ibg() {
+        let ibg = document.querySelectorAll(".ibg");
+        for (var i = 0; i < ibg.length; i++) {
+            if (ibg[i].querySelector('img')) {
+                ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+            }
+        }
+    }
+
+    ibg();
 });
